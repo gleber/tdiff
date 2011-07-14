e3218197f76cd355a41e8cc726238d6d0cc5568a
Gleb Peregud <gleber.p@gmail.com>
2011-07-14 23:44:28 +0200

    get rid of some code make diff work with tuples, where first element is actual value, second element is opaque annotation data

--- src/tdiff.erl
+++ src/tdiff.erl
@@ -20,64 +20,7 @@
 %%%
 
 -module(tdiff).
--export([diff/2, diff/3, patch/2]).
--export([diff_files/2, diff_files/3]).
--export([diff_binaries/2, diff_binaries/3]).
--export([format_diff_lines/1]).
--export([print_diff_lines/1]).
-
-diff_files(F1, F2) -> diff_files(F1, F2, _Opts=[]).
-diff_files(F1, F2, Opts) ->
-    {ok,B1} = file:read_file(F1),
-    {ok,B2} = file:read_file(F2),
-    diff_binaries(B1, B2, Opts).
-
-diff_binaries(B1, B2) -> diff_binaries(B1, B2, _Opts=[]).
-diff_binaries(B1, B2, Opts) ->
-    diff(split_bin_to_lines(B1), split_bin_to_lines(B2), Opts).
-
-
-split_bin_to_lines(B) -> sbtl(binary_to_list(B), "", []).
-
-sbtl("\n" ++ Rest, L, Acc) -> sbtl(Rest, "", [lists:reverse("\n"++L) | Acc]);
-sbtl([C|Rest], L, Acc)     -> sbtl(Rest, [C|L], Acc);
-sbtl("", "", Acc)          -> lists:reverse(Acc);
-sbtl("", L, Acc)           -> lists:reverse([lists:reverse(L) | Acc]).
-
-
-print_diff_lines(Diff) -> io:format("~s~n", [format_diff_lines(Diff)]).
-
-format_diff_lines(Diff) -> fdl(Diff, 1,1).
-
-fdl([{del,Ls1},{ins,Ls2}|T], X, Y) ->
-    Addr = io_lib:format("~sc~s~n", [fmt_addr(X,Ls1), fmt_addr(Y, Ls2)]),
-    Del  = format_lines("< ", Ls1),
-    Sep  = io_lib:format("---~n", []),
-    Ins  = format_lines("> ", Ls2),
-    [Addr, Del, Sep, Ins | fdl(T, X+length(Ls1), Y+length(Ls2))];
-fdl([{del,Ls}|T], X, Y) ->
-    Addr = io_lib:format("~w,~wd~w~n", [X,X+length(Ls), Y]),
-    Del  = format_lines("< ", Ls),
-    [Addr, Del | fdl(T, X+length(Ls), Y)];
-fdl([{ins,Ls}|T], X, Y) ->
-    Addr = io_lib:format("~wa~w,~w~n", [X,Y,Y+length(Ls)]),
-    Ins  = format_lines("> ", Ls),
-    [Addr, Ins | fdl(T, X, Y+length(Ls))];
-fdl([{eq,Ls}|T], X, Y) ->
-    fdl(T, X+length(Ls), Y+length(Ls));
-fdl([], _X, _Y) ->
-    [].
-
-fmt_addr(N, Ls) when length(Ls) == 1 -> f("~w",    [N]);
-fmt_addr(N, Ls)                      -> f("~w,~w", [N,N+length(Ls)-1]).
-
-f(F,A) -> lists:flatten(io_lib:format(F,A)).
-
-format_lines(Indicator, Lines) ->
-    lists:map(fun(Line) -> io_lib:format("~s~s", [Indicator, Line]) end,
-              Lines).
-
-
+-export([diff/2]).
 
 
 %%---------------------------------------------------------------------
@@ -169,31 +112,26 @@ format_lines(Indicator, Lines) ->
 %% good, because this fits the way lists are built in functional
 %% programming languages.
 
-diff(Sx, Sy) -> diff(Sx, Sy, _Opts=[]).
-
-diff(Sx, Sy, Opts) ->
+diff(Sx, Sy) ->
     SxLen = length(Sx),
     SyLen = length(Sy),
     DMax = SxLen + SyLen,
-    Tracer = proplists:get_value(algorithm_tracer, Opts, no_tracer),
-    EditScript = case try_dpaths(0, DMax, [{0, 0, Sx, Sy, []}], Tracer) of
+    EditScript = case try_dpaths(0, DMax, [{0, 0, Sx, Sy, []}]) of
                      no            -> [{del,Sx},{ins,Sy}];
                      {ed,EditOpsR} -> edit_ops_to_edit_script(EditOpsR)
                  end,
-    t_final_script(Tracer, EditScript),
     EditScript.
 
 
-try_dpaths(D, DMax, D1Paths, Tracer) when D =< DMax ->
-    t_d(Tracer, D),
-    case try_kdiagonals(-D, D, D1Paths, [], Tracer) of
+try_dpaths(D, DMax, D1Paths) when D =< DMax ->
+    case try_kdiagonals(-D, D, D1Paths, []) of
         {ed, E}          -> {ed, E};
-        {dpaths, DPaths} -> try_dpaths(D+1, DMax, DPaths, Tracer)
+        {dpaths, DPaths} -> try_dpaths(D+1, DMax, DPaths)
     end;
-try_dpaths(_, _DMax, _DPaths, _Tracer) ->
+try_dpaths(_, _DMax, _DPaths) ->
     no.
 
-try_kdiagonals(K, D, D1Paths, DPaths, Tracer) when K =< D ->
+try_kdiagonals(K, D, D1Paths, DPaths) when K =< D ->
     DPath = if D == 0 -> hd(D1Paths);
                true   -> pick_best_dpath(K, D, D1Paths)
             end,
@@ -201,18 +139,17 @@ try_kdiagonals(K, D, D1Paths, DPaths, Tracer) when K =< D ->
         {ed, E} ->
             {ed, E};
         {dpath, DPath2} when K =/= -D ->
-            t_dpath(Tracer, DPath2),
-            try_kdiagonals(K+2, D, tl(D1Paths), [DPath2 | DPaths], Tracer);
+            try_kdiagonals(K+2, D, tl(D1Paths), [DPath2 | DPaths]);
         {dpath, DPath2} when K =:= -D ->
-            t_dpath(Tracer, DPath2),
-            try_kdiagonals(K+2, D, D1Paths, [DPath2 | DPaths], Tracer)
+            try_kdiagonals(K+2, D, D1Paths, [DPath2 | DPaths])
     end;
-try_kdiagonals(_, D, _, DPaths, Tracer) ->
-    t_exhausted_kdiagonals(Tracer, D),
+try_kdiagonals(_, _D, _, DPaths) ->
     {dpaths, lists:reverse(DPaths)}.
 
-follow_snake({X, Y, [H|Tx], [H|Ty], Cs}) -> follow_snake({X+1,Y+1, Tx,Ty,
-                                                          [{e,H} | Cs]});
+follow_snake({X, Y,
+              [{Val, Ann }|Tx],
+              [{Val, Ann2}|Ty], Cs}) -> follow_snake({X+1,Y+1, Tx,Ty,
+                                                            [{e,{Val,[Ann,Ann2]}} | Cs]});
 follow_snake({_X,_Y,[],     [],     Cs}) -> {ed, Cs};
 follow_snake({X, Y, [],     Sy,     Cs}) -> {dpath, {X, Y, [],  Sy,  Cs}};
 follow_snake({X, Y, oob,    Sy,     Cs}) -> {dpath, {X, Y, oob, Sy,  Cs}};
@@ -240,40 +177,10 @@ go_inc_x({X, Y, Sx, oob,    Cs}) -> {X+1, Y, Sx,  oob, Cs}.
 
 edit_ops_to_edit_script(EditOps) -> e2e(EditOps, _Acc=[]).
 
-e2e([{x,C}|T], [{ins,R}|Acc]) -> e2e(T, [{ins,[C|R]}|Acc]);
-e2e([{y,C}|T], [{del,R}|Acc]) -> e2e(T, [{del,[C|R]}|Acc]);
-e2e([{e,C}|T], [{eq,R}|Acc])  -> e2e(T, [{eq, [C|R]}|Acc]);
-e2e([{x,C}|T], Acc)           -> e2e(T, [{ins,[C]}|Acc]);
-e2e([{y,C}|T], Acc)           -> e2e(T, [{del,[C]}|Acc]);
-e2e([{e,C}|T], Acc)           -> e2e(T, [{eq, [C]}|Acc]);
-e2e([],        Acc)           -> Acc.
-
-
-patch(S, Diff) -> p2(S, Diff, []).
-
-p2(S, [{eq,T}|Rest], Acc)  -> p2_eq(S, T, Rest, Acc);
-p2(S, [{ins,T}|Rest], Acc) -> p2_ins(S, T, Rest, Acc);
-p2(S, [{del,T}|Rest], Acc) -> p2_del(S, T, Rest, Acc);
-p2([],[], Acc)             -> lists:reverse(Acc).
-
-p2_eq([H|S], [H|T], Rest, Acc) -> p2_eq(S, T, Rest, [H|Acc]);
-p2_eq(S,     [],    Rest, Acc) -> p2(S, Rest, Acc).
-
-p2_ins(S, [H|T], Rest, Acc) -> p2_ins(S, T, Rest, [H|Acc]);
-p2_ins(S, [],    Rest, Acc) -> p2(S, Rest, Acc).
-
-p2_del([H|S], [H|T], Rest, Acc) -> p2_del(S, T, Rest, Acc);
-p2_del(S,     [],    Rest, Acc) -> p2(S, Rest, Acc).
-
-
-t_final_script(no_tracer, _)       -> ok;
-t_final_script(Tracer, EditScript) -> Tracer({final_edit_script, EditScript}).
-
-t_d(no_tracer, _) -> ok;
-t_d(Tracer, D)    -> Tracer({d,D}).
-
-t_dpath(no_tracer, _)  -> ok;
-t_dpath(Tracer, DPath) -> Tracer({dpath,DPath}).
-
-t_exhausted_kdiagonals(no_tracer, _) -> ok;
-t_exhausted_kdiagonals(Tracer, D)    -> Tracer({exhausted_kdiagonals, D}).
+e2e([{x,C}|T], [{ins,R}|Acc])        -> e2e(T, [{ins,[C|R]}|Acc]);
+e2e([{y,C}|T], [{del,R}|Acc])        -> e2e(T, [{del,[C|R]}|Acc]);
+e2e([{e,{V,Anns}}|T], [{eq,R}|Acc])  -> e2e(T, [{eq, [{V,Anns}|R]}|Acc]);
+e2e([{x,C}|T], Acc)                  -> e2e(T, [{ins,[C]}|Acc]);
+e2e([{y,C}|T], Acc)                  -> e2e(T, [{del,[C]}|Acc]);
+e2e([{e,{V,Anns}}|T], Acc)           -> e2e(T, [{eq, [{V,Anns}]}|Acc]);
+e2e([],        Acc)                  -> Acc.
